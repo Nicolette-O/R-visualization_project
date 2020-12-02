e6f7cfbe76b16f0feeeb2f786749e65505f985fc
Nicolette-O <75082332+Nicolette-O@users.noreply.github.com>
2020-12-01 23:11:48 -0600

    Update pheatmap.R

--- pheatmap.R
+++ pheatmap.R
@@ -1,9 +1,22 @@
-#install packages
 
+#install packages
 install.packages("pheatmap")
+install.packages("readxl")
+
+#imort data
+library(readxl)
+GSE120277_RNAseq_data <- read_excel("file directory", 
+    sheet = "microRNA")
+View(GSE120277_RNAseq_data)
+
+#convert to numeric scale
+miRNA <- GSE120277_RNAseq_data[2:10]
+miRNAheat <-scale(miRNA)
+
+#convert to matrix
 miRNAheat <- as.matrix(miRNA)
 
 library(pheatmap) 
 
-#plot heatmap
+#plot pheatmap heatmap
 pheatmap(miRNAheat, scale = "row")
