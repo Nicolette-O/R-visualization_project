83a378b2651736dc0febcf89989956f36e9db77e
Nicolette-O <75082332+Nicolette-O@users.noreply.github.com>
2020-12-01 23:13:09 -0600

    Update heat_maps.R

--- heat_maps.R
+++ heat_maps.R
@@ -1,5 +1,8 @@
 #import RNAseq data
-GSE120277_RNAseq_data 
+library(readxl)
+GSE120277_RNAseq_data <- read_excel("excel file directory", 
+    sheet = "microRNA")
+View(GSE120277_RNAseq_data)
 
 #convert to numerical and scale
 miRNA <- GSE120277_RNAseq_data[2:10]
