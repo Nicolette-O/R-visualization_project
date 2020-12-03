ca5b845945e27fbc421aefc07bc14eed2b15ac4c
Nicolette-O <75082332+Nicolette-O@users.noreply.github.com>
2020-12-02 20:42:56 -0600

    Update corrplots.R

--- corrplots.R
+++ corrplots.R
@@ -1,1 +1,11 @@
+#install corrplot packages
+install.packages("corrplot")
+library(corrplot)
 
+#import data
+
+miRNA <- GSE120277_RNAseq_data
+
+#corrplot
+miRNA.cor=cor(miRNA[2:10])
+corrplot(miRNA.cor)
